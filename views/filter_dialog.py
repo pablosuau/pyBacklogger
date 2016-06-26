417af03d36f0e5d741d337e24569ccaad4dcb21e
Pablo Suau <pablosuau@gmail.com>
2016-06-26 11:19:46 +0100

    Minor fixes to the filter dialog view

--- views/filter_dialog.py
+++ views/filter_dialog.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'filter_dialog.ui'
 #
-# Created: Sun Jun 26 11:15:03 2016
+# Created: Sun Jun 26 11:18:59 2016
 #      by: PyQt4 UI code generator 4.11.3
 #
 # WARNING! All changes made in this file will be lost!
@@ -41,6 +41,9 @@ class Ui_FilterDialog(object):
         self.pushButtonSelectAllSystem = QtGui.QPushButton(self.tabSystem)
         self.pushButtonSelectAllSystem.setGeometry(QtCore.QRect(10, 7, 75, 23))
         self.pushButtonSelectAllSystem.setObjectName(_fromUtf8("pushButtonSelectAllSystem"))
+        self.listSystem = QtGui.QListView(self.tabSystem)
+        self.listSystem.setGeometry(QtCore.QRect(10, 40, 231, 241))
+        self.listSystem.setObjectName(_fromUtf8("listSystem"))
         self.tabCategory.addTab(self.tabSystem, _fromUtf8(""))
         self.tabStatus = QtGui.QWidget()
         self.tabStatus.setObjectName(_fromUtf8("tabStatus"))
@@ -50,6 +53,9 @@ class Ui_FilterDialog(object):
         self.pushButtonSelectAllStatus = QtGui.QPushButton(self.tabStatus)
         self.pushButtonSelectAllStatus.setGeometry(QtCore.QRect(10, 7, 75, 23))
         self.pushButtonSelectAllStatus.setObjectName(_fromUtf8("pushButtonSelectAllStatus"))
+        self.listStatus = QtGui.QListView(self.tabStatus)
+        self.listStatus.setGeometry(QtCore.QRect(10, 40, 231, 241))
+        self.listStatus.setObjectName(_fromUtf8("listStatus"))
         self.tabCategory.addTab(self.tabStatus, _fromUtf8(""))
         self.tabLabel = QtGui.QWidget()
         self.tabLabel.setObjectName(_fromUtf8("tabLabel"))
@@ -59,6 +65,9 @@ class Ui_FilterDialog(object):
         self.pushButtonSelectAllLabel = QtGui.QPushButton(self.tabLabel)
         self.pushButtonSelectAllLabel.setGeometry(QtCore.QRect(10, 7, 75, 23))
         self.pushButtonSelectAllLabel.setObjectName(_fromUtf8("pushButtonSelectAllLabel"))
+        self.listLabel = QtGui.QListView(self.tabLabel)
+        self.listLabel.setGeometry(QtCore.QRect(10, 40, 231, 241))
+        self.listLabel.setObjectName(_fromUtf8("listLabel"))
         self.tabCategory.addTab(self.tabLabel, _fromUtf8(""))
         self.pushButtonCancel = QtGui.QPushButton(FilterDialog)
         self.pushButtonCancel.setGeometry(QtCore.QRect(170, 330, 75, 23))
@@ -66,9 +75,6 @@ class Ui_FilterDialog(object):
         self.pushButtonOk = QtGui.QPushButton(FilterDialog)
         self.pushButtonOk.setGeometry(QtCore.QRect(90, 330, 75, 23))
         self.pushButtonOk.setObjectName(_fromUtf8("pushButtonOk"))
-        self.listLabel = QtGui.QListView(FilterDialog)
-        self.listLabel.setGeometry(QtCore.QRect(10, 60, 231, 241))
-        self.listLabel.setObjectName(_fromUtf8("listLabel"))
 
         self.retranslateUi(FilterDialog)
         self.tabCategory.setCurrentIndex(2)
