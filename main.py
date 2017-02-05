22b0ad408e56127fa27243ede9da87da0cd12362
Pablo Suau <pablosuau@gmail.com>
2017-02-05 11:21:17 +0000

    Fix: tha application's icon is also displayed on windows' taskbar

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from controllers.main_window_controller import *
 from PyQt4 import QtGui
 import sys
+import ctypes
+
+myappid = 'pablosuau.pybacklogger' 
+ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
         
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
