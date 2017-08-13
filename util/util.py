f1b118a40da3d5f1186d818ee482317a3425ff34
Pablo Suau <pablosuau@gmail.com>
2017-08-13 07:56:51 +0100

    Fix: make error messages modal. Fixes #11

--- util/util.py
+++ util/util.py
@@ -1,7 +1,8 @@
-from PyQt4 import QtGui
+from PyQt4 import QtGui, QtCore
 
 # Notification methods
 def showErrorMessage(widget, message):
     errorMessage = QtGui.QErrorMessage(widget)
+    errorMessage.setWindowModality(QtCore.Qt.WindowModal)
     errorMessage.setWindowTitle(widget.windowTitle())
     errorMessage.showMessage(message)
